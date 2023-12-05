0dffb8dbe3c6146f9b7d45d94319b7a4b3f9debe
amethystgnome <sample.aubry@gmail.com>
2023-12-04 23:42:20 -0800

    output on website not working currently

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from mongoengine import Document, StringField, connect
-
+import joblib
+import spam_classifier
 
 app = Flask(__name__)
 
@@ -13,31 +14,57 @@ class Email(Document):
     content = StringField(required=True)
     spam_or_real = StringField()
 
-# prints emails for test
-for email in Email.objects:
-    print(email.subject, email.content)
+# Function to load model and vectorizer
+def load_model_and_vectorizer():
+    try:
+        model = joblib.load('model.pkl')
+        vectorizer = joblib.load('feature_extraction.pkl')
+        return model, vectorizer
+    except FileNotFoundError:
+        print("Model or vectorizer files not found. Ensure they are in the correct directory.")
+        return None, None
 
+model, vectorizer = load_model_and_vectorizer()
+
+@app.route('/train-model', methods=['GET', 'POST'])
+def train_model():
+    if request.method == 'POST':
+
+        spam_classifier.train()
+        # After retraining, reload the model and vectorizer
+        global model, vectorizer
+        model, vectorizer = load_model_and_vectorizer()
+        return "Model retrained successfully", 200
+    return render_template('train-model.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         subject = request.form['subject']
         content = request.form['content']
-        entry = Email(subject= subject, content= content, spam_or_real = "True")
+        full_email = "subject: " + subject + "." + content
+
+        if model and vectorizer:
+            input_data_features = vectorizer.transform([full_email])
+            prediction = model.predict(input_data_features)
+            spam_or_real = "Spam" if prediction[0] == 0 else "Real"
+        else:
+            spam_or_real = "Error"
+
+        # Save email details to database
+        entry = Email(subject=subject, content=content, spam_or_real=spam_or_real)
         entry.save()
-        full_email = "subject:" + subject + "." + content
-        # Handle the submitted data (save, send, process, etc.)
-        # For now, just redirect to the home page
+        
         return redirect('/')
     return render_template('index.html')
-
 
 @app.route("/emails")
 def print_emails():
     emails = Email.objects.all()
-
     return render_template('emails.html', emails=emails)
-    
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
