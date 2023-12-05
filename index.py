7023c4b9fb3c60ac1d1803f5ede0c6112de86d83
dustinchavez1 <54484108+dustinchavez1@users.noreply.github.com>
2023-12-04 21:10:06 -0800

    Update index.py
    update db and routes to handle forms

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from mongoengine import Document, StringField, connect
 
 
@@ -9,16 +9,26 @@ connect('emails', host='localhost', port=27017)
 
 # Creates schema for database
 class Email(Document):
-    title = StringField(required=True)
+    subject = StringField(required=True)
     content = StringField(required=True)
+    spam_or_real = StringField()
 
 # prints emails for test
 for email in Email.objects:
-    print(email.title, email.content)
+    print(email.subject, email.content)
 
 
-@app.route("/")
-def home():
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        subject = request.form['subject']
+        content = request.form['content']
+        entry = Email(subject= subject, content= content, spam_or_real = "True")
+        entry.save()
+        full_email = "subject:" + subject + "." + content
+        # Handle the submitted data (save, send, process, etc.)
+        # For now, just redirect to the home page
+        return redirect('/')
     return render_template('index.html')
 
 
@@ -28,5 +38,8 @@ def print_emails():
 
     return render_template('emails.html', emails=emails)
     
+
+
+
 
 
