291b055c30451627e6eacbf8e305ffbed59b9402
dustinchavez1 <54484108+dustinchavez1@users.noreply.github.com>
2023-12-05 00:31:28 -0800

    Working to correctly add spam or real email to /emails

--- index.py
+++ index.py
@@ -14,42 +14,13 @@ class Email(Document):
     content = StringField(required=True)
     spam_or_real = StringField()
 
-# Function to load model and vectorizer
-def load_model_and_vectorizer():
-    try:
-        model = joblib.load('model.pkl')
-        vectorizer = joblib.load('feature_extraction.pkl')
-        return model, vectorizer
-    except FileNotFoundError:
-        print("Model or vectorizer files not found. Ensure they are in the correct directory.")
-        return None, None
-
-model, vectorizer = load_model_and_vectorizer()
-
-@app.route('/train-model', methods=['GET', 'POST'])
-def train_model():
-    if request.method == 'POST':
-
-        spam_classifier.train()
-        # After retraining, reload the model and vectorizer
-        global model, vectorizer
-        model, vectorizer = load_model_and_vectorizer()
-        return "Model retrained successfully", 200
-    return render_template('train-model.html')
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         subject = request.form['subject']
         content = request.form['content']
         full_email = "subject: " + subject + "." + content
-
-        if model and vectorizer:
-            input_data_features = vectorizer.transform([full_email])
-            prediction = model.predict(input_data_features)
-            spam_or_real = "Spam" if prediction[0] == 0 else "Real"
-        else:
-            spam_or_real = "Error"
+        spam_or_real = spam_classifier.prediction(full_email)
 
         # Save email details to database
         entry = Email(subject=subject, content=content, spam_or_real=spam_or_real)
