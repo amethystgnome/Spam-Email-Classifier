055bdc1f10e148169b7f96ff1f320d4c92c3a012
tireinny <charlestuong150@gmail.com>
2023-12-05 15:27:57 -0800

    styling finally works. might do more styling later

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from mongoengine import Document, StringField, connect
 import joblib
 import spam_classifier
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = 'templates', static_folder = 'styles')
 
 model = None
 vectorizer = None
@@ -25,7 +25,7 @@ def load_model_and_vectorizer():
 
 load_model_and_vectorizer()
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods = ['GET', 'POST'])
 def index():
     global model, vectorizer
     if request.method == 'POST':
@@ -40,7 +40,7 @@ def index():
             spam_or_real = "Error"
 
         # Save email details to database
-        entry = Email(subject=subject, content=content, spam_or_real=spam_or_real)
+        entry = Email(subject = subject, content = content, spam_or_real = spam_or_real)
         entry.save()
         
         return redirect('/emails')
@@ -49,10 +49,10 @@ def index():
 @app.route("/emails")
 def print_emails():
     emails = Email.objects.all()
-    return render_template('emails.html', emails=emails)
+    return render_template('emails.html', emails = emails)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
 
 
 
